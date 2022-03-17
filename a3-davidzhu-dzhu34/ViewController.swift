9340149983206481a05e5fd00712077c70fe002f
David Zhu <dzhu34@myseneca.ca>
2022-03-16 21:59:22 -0400

    Initial Commit

---
+++ a3-davidzhu-dzhu34/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  a3-davidzhu-dzhu34
+//
+//  Created by showtimez on 2022-03-16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
